5720c18aee86188473cbfefb356522e613537a3b
TarunPeela <127425166+TarunVirat@users.noreply.github.com>
2025-09-30 15:14:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
 import streamlit as st
 import numpy as np
 import pickle
-
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.src.legacy.saving import legacy_h5_format
-#Load the LSTM Model
-model = legacy_h5_format.load_model_from_hdf5('next_word_lstm.h5')
+from keras import layers
 
-#3 Laod the tokenizer
-with open('tokenizer.pickle','rb') as handle:
-    tokenizer=pickle.load(handle)
+# Load the LSTM Model with custom objects
+custom_objects = {
+    "LSTM": layers.LSTM,
+    "GRU": layers.GRU,
+    "SimpleRNN": layers.SimpleRNN,
+    "Embedding": layers.Embedding,
+    "Dense": layers.Dense,
+    "Dropout": layers.Dropout
+}
+
+model = legacy_h5_format.load_model_from_hdf5(
+    'next_word_lstm.h5',
+    custom_objects=custom_objects
+)
+
+# Load the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 # Function to predict the next word
 def predict_next_word(model, tokenizer, text, max_sequence_len):
     token_list = tokenizer.texts_to_sequences([text])[0]
     if len(token_list) >= max_sequence_len:
-        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
-    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+        token_list = token_list[-(max_sequence_len - 1):]  # Ensure sequence length matches max_sequence_len-1
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
     predicted = model.predict(token_list, verbose=0)
     predicted_word_index = np.argmax(predicted, axis=1)
     for word, index in tokenizer.word_index.items():
@@ -24,12 +37,10 @@ def predict_next_word(model, tokenizer, text, max_sequence_len):
             return word
     return None
 
-# streamlit app
+# Streamlit app
 st.title("Next Word Prediction With LSTM And Early Stopping")
-input_text=st.text_input("Enter the sequence of Words","To be or not to")
+input_text = st.text_input("Enter the sequence of Words", "To be or not to")
 if st.button("Predict Next Word"):
     max_sequence_len = model.input_shape[1] + 1  # Retrieve the max sequence length from the model input shape
     next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
     st.write(f'Next word: {next_word}')
-
-
